6e27c3a7317a9414313599d7be58c77c18e9b7a8
rexos <alex.pellegrini@live.com>
2013-11-19 02:51:37 +0100

    installation downloads source code and cleans

--- ds/install.sh
+++ ds/install.sh
@@ -1,17 +1,25 @@
 #! /bin/bash
 name=""
 
-if [ -f ds_source.c ] &&
+if [ -e /usr/bin/curl ]; then
+    curl https://raw.github.com/rexos/unix/master/ds/ds_source.c > source.c
+else
+    wget https://raw.github.com/rexos/unix/master/ds/ds_source.c > source.c
+fi
+
+if [ -f source.c ] &&
     [ -e /usr/bin/gcc ]; then
-    
+  
     echo "command name : "
     read name
-    gcc ds_source.c -o $name
-    sudo cp ./$name /bin/$name
-    rm ./ds
+    gcc source.c -o $name
+    sudo cp ./$name /bin/$name 
     if [ $? == 0 ]; then
 	echo "$name successfully installed"
     else
 	echo "error while installing $name"
     fi
+    echo "cleaning ..."
+    rm ./$name
+    rm ./source.c
 fi
