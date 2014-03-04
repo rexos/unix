458674b84e0bf6a18f5d5a4f3169d4836fb818a3
rexos <alex.pellegrini@live.com>
2014-03-04 23:05:07 +0100

    restructured some recursions

--- lisp/length.lisp
+++ lisp/length.lisp
@@ -2,6 +2,13 @@
   (if (null x) 0 
     (1+ (list-len (rest x)))))
 
+;; allows tail recursion
+(defun fast-length (L)
+  (fast-length-aux L 0))
+(defun fast-length-aux (L n) 
+  (if (null L) n 
+    (fast-length-aux (rest L) (1+ n))))
+
 (defun sumup (L)
   (if (null L) 0
     (+ (first L) (sumup (rest L)))))
--- lisp/power.lisp
+++ lisp/power.lisp
@@ -1,2 +1,10 @@
 (defun power (x y) (if (= y 0) 1 
 		     (* x (power x (- y 1)))))
+
+;; allows tail recursion
+
+(defun fast-power (x y) 
+  (fast-power-aux x y 1))
+(defun fast-power-aux (x y n) 
+  (if (= y 0) n 
+    (fast-power-aux x (1- y) (* x n))))
